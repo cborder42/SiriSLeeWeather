e1deebab45ebe045e87f694cee951e226d4fa392
hpark12345 <88914754+hpark12345@users.noreply.github.com>
2022-06-01 09:02:49 -0700

    Update Siri.java
    - the graphic
    
    - the interface with the submit button isn't updated I just want to see if it can be put on the graphic

--- Siri.java
+++ Siri.java
@@ -6,12 +6,17 @@ import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.event.*;a
 import javax.swing.*;
-import java.awt.Color;
 
-public class Siri extends JFrame {
+public class DrawShapes extends JFrame {
 
-	public Siri() {
+	public DrawShapes() {
 
 		setSize(new Dimension(500, 500));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,16 +26,36 @@ public class Siri extends JFrame {
 			public void paintComponent(Graphics g) {
 				Graphics2D f = (Graphics2D) g;
 				Shape rect = new Rectangle(75, 10, 350, 450);
-            Shape rect2 = new RoundRectangle2D.Double(200, 55, 100, 15,15,50);
 				Shape circle = new Ellipse2D.Double(200, 325, 100, 100);
-            Shape circle2 = new Ellipse2D.Double(280, 58, 10, 10);
 				Shape roundRect = new RoundRectangle2D.Double(100, 50, 300, 400, 30, 50);
 				f.draw(rect);
-            f.draw(rect2);
 				f.draw(circle);
-            f.draw(circle2);
 				f.draw(roundRect);
+            
+            JFrame e = new JFrame("Siri Weather");
+   //set size and location of frame
+   e.setSize(390, 300);
+   e.setLocation(100, 150);
+   //make sure it quits when x is clicked
+   e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   //set look and feel
+   e.setDefaultLookAndFeelDecorated(true);
+   JLabel label = new JLabel("Enter City");
+   label.setBounds(50, 50, 200, 30);
+   JTextField motto1 = new JTextField();
+   //set size of the text box
+   motto1.setBounds(50, 100, 200, 30);
+   //add elements to the frame
+   e.add(label);
+   e.add(motto1);
+   e.setLayout(null);
+   e.setVisible(true);
    
+   //add a button
+JButton b = new JButton("Submit");
+b.setBounds(50, 150, 100, 30);
+//add button to the frame
+e.add(b);
     			}
 		};
 		setTitle("Siri Weather");
@@ -42,11 +67,11 @@ public class Siri extends JFrame {
 		SwingUtilities.invokeLater(new Runnable() {
 
 			public void run() {
-				new Siri();
+				new DrawShapes();
 			}
 		});
    }
    
-
+   
 
 }
